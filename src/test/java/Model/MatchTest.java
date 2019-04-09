4cba9826662faa7d48f9ae123ff4ea5fd4fc0a87
unknown <angelica.valeriani@mail.polimi.it>
2019-04-09 20:28:41 +0200

    Aggiunto test per Match

---
+++ src/test/java/Model/MatchTest.java
@@ -0,0 +1,63 @@
+package Model;
+
+import exceptions.MaxNumberPlayerException;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MatchTest {
+    Match match;
+    Player player1;
+    Player player2;
+    Player player3;
+    Player player4;
+
+
+    @Before
+    public void SetUp(){
+        match = new Match();
+        player1 = new Player("Sirius", "blue", "10583741");
+        player2 = new Player("Calypso", "pink", "14253954");
+        player3 = new Player("Hermione", "green", "18263100");
+        player4 = new Player("Aries", "yellow", "18992302");
+        try {
+            match.add_player(player1);
+            match.add_player(player2);
+            match.add_player(player3);
+            match.add_player(player4);
+        }
+        catch (MaxNumberPlayerException e){}
+    }
+
+    @Test
+    public void set_round() {
+        assertEquals(1, match.get_round());
+        Player player5 = new Player("Karka", "grey", "18114320");
+        try {
+            match.add_player(player5);
+        }
+        catch (MaxNumberPlayerException e){}
+        player5.set_action();
+        player5.set_action();
+        match.set_round();
+        assertEquals(2, match.get_round());
+    }
+
+    @Test
+    public void get_round() {
+        assertEquals(1, match.get_round());
+    }
+
+    @Test
+    public void add_player() {
+        Player player5 = new Player("Karka", "grey", "18114320");
+        Player player6 = new Player("Gemini", "grey", "10003256");
+        try {
+            match.add_player(player5);
+        }
+        catch (MaxNumberPlayerException e){}
+        assertThrows(MaxNumberPlayerException.class, () -> match.add_player(player6));
+    }
+}
