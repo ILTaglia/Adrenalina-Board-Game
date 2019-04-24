5b3efb1ae6f62868b112c993862d1064d6ac5f6d
Daniele Tagliabue <daniele1.tagliabue@mail.polimi.it>
2019-04-24 11:10:24 +0200

    "Aggiunta Model dei potenziamenti e sistemato file armi"

---
+++ src/main/java/Model/PowCard.java
@@ -0,0 +1,16 @@
+package Model;
+
+public interface PowCard {
+    public void setColor(int color);
+    public int getColor();
+    public void setCost(int cost);
+    public int getCost();
+    public String getName();
+    public void setMoveme(int moveme);
+    public int getMoveme();
+    public void setLife(int life);
+    public int getLife();
+    public void setMarks(int marks);
+    public int getMarks();
+
+}
---
+++ src/main/java/Model/Teleporter.java
@@ -0,0 +1,69 @@
+package Model;
+
+public class Teleporter implements PowCard{
+    private int color;
+    private int cost;
+    private int moveme;
+    private int life;
+    private int marks;
+    public Teleporter(int color, int cost, int moveme, int life, int marks)
+    {
+        this.color=color;
+        this.cost=cost;
+        this.moveme=moveme;
+        this.life=life;
+        this.marks=marks;
+    }
+    public Teleporter(int color, int cost)
+    {
+        this.color=color;
+        this.cost=cost;
+        this.moveme=0;
+        this.life=0;
+        this.marks=0;
+    }
+    public void setColor(int color)
+    {
+        this.color=color;
+    }
+    public int getColor()
+    {
+        return this.color;
+    }
+    public void setCost(int cost)
+    {
+        this.cost=cost;
+    }
+    public int getCost()
+    {
+        return this.cost;
+    }
+    public String getName()
+    {
+        return "Teletrasporto";
+    }
+    public void setMoveme(int moveme)
+    {
+        this.moveme=moveme;
+    }
+    public int getMoveme()
+    {
+        return this.moveme;
+    }
+    public void setLife(int life)
+    {
+        this.life=life;
+    }
+    public int getLife()
+    {
+        return this.life;
+    }
+    public void setMarks(int marks)
+    {
+        this.marks=marks;
+    }
+    public int getMarks()
+    {
+        return this.marks;
+    }
+}
