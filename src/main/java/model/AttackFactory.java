bbdbc91ea696945a915a1299ff969f74ca6069de
ILTaglia <48481804+ILTaglia@users.noreply.github.com>
2019-05-14 12:10:32 +0200

    Creazione classi aggiuntive per attacco (Senza era diventato quasi impossibile gestire gli attacchi nel controller)

--- src/main/java/model/AttackFactory.java
+++ src/main/java/model/AttackFactory.java
@@ -7,17 +7,50 @@ public class AttackFactory {
         {
             return new FiniteDistance(typeplayer,distance,moveme,moveyou);
         }
-        else
-            if(1==2)
-            {
-                return new UndefinedDistance(typeplayer,distance,moveme,moveyou);
-            }
-            else
-                if(i==3)
-                {
-                    return new MoreDistance(typeplayer,distance,moveme,moveyou);
-                }
-                else
-                    return new Cardinal(typeplayer,distance,moveme,moveyou);
+        if(i==2)
+        {
+            return new UndefinedDistance(typeplayer,distance,moveme,moveyou);
+        }
+        if(i==2)
+        {
+            return new MoreDistance(typeplayer,distance,moveme,moveyou);
+        }
+        if(i==2)
+        {
+            return new Cardinal(typeplayer,distance,moveme,moveyou);
+        }
+        if(i==2)
+        {
+            return new NotSeen(typeplayer,distance,moveme,moveyou);
+        }
+        if(i==2)
+        {
+            return new WhileMoving(typeplayer,distance,moveme,moveyou);
+        }
+        if(i==2)
+        {
+            return new AllAround(typeplayer,distance,moveme,moveyou);
+        }
+        if(i==2)
+        {
+            return new AfterMoving(typeplayer,distance,moveme,moveyou);
+        }
+        if(i==2)
+        {
+            return new AllRoom(typeplayer,distance,moveme,moveyou);
+        }
+        if(i==2)
+        {
+            return new Ricorsive(typeplayer,distance,moveme,moveyou);
+        }
+        if(i==2)
+        {
+            return new InFiniteLine(typeplayer,distance,moveme,moveyou);
+        }
+        if(i==2)
+        {
+            return new MovingToMe(typeplayer,distance,moveme,moveyou);
+        }
+        return null;
     }
 }
---
+++ src/main/java/model/Ricorsive.java
@@ -0,0 +1,80 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Ricorsive implements TypeAttack {
+    private ArrayList<Integer> extra;
+    private int typeplayer;
+    private int distance;
+    private int moveme;
+    private int moveyou;
+    private ArrayList<Effect> E;
+    public int getDistance()
+    {
+        return this.distance;
+    }
+    public int getTypePlayer()
+    {
+        return this.typeplayer;
+    }
+    public ArrayList getExtras()
+    {
+        return this.extra;
+    }
+    public void setDistance(int distance)
+    {
+        this.distance=distance;
+    }
+    public void setTypePlayer(int typePlayer)
+    {
+        this.typeplayer= typePlayer;
+    }
+    public void addExtra(int extra)
+    {
+        this.extra.add(extra);
+    }
+    public void setMoveMe(int moveMe)
+    {
+        this.moveme= moveMe;
+    }
+    public void setMoveYou(int moveYou)
+    {
+        this.moveyou= moveYou;
+    }
+    public void addEffect(Effect E)
+    {
+        this.E.add(E);
+    }
+    public Effect getEffect(int i)
+    {
+        return this.E.get(i);
+    }
+    public int getNumberEffect()
+    {
+        return this.E.size();
+    }
+    public int getNumberExtra()
+    {
+        return this.extra.size();
+    }
+    public int getMoveMe()
+    {
+        return this.moveme;
+    }
+    public int getMoveYou()
+    {
+        return this.moveyou;
+    }
+    public int getType(){return 0;}
+
+
+    public Ricorsive(int typeplayer, int distance, int moveme, int moveyou)
+    {
+        this.extra=new ArrayList<Integer>();
+        this.E= new ArrayList<Effect>();
+        this.distance=distance;
+        this.moveme=moveme;
+        this.moveyou=moveyou;
+        this.typeplayer=typeplayer;
+    }
+}
