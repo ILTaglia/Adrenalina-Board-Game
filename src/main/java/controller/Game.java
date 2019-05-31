33e6173b6a21656fe46b8a9b91bbd27bda94326b
samuele-panzeri <samuele1.panzeri@mail.polimi.it>
2019-05-31 07:51:21 +0200

    Modifiche minori

--- src/main/java/controller/Game.java
+++ src/main/java/controller/Game.java
@@ -16,62 +16,23 @@ import java.util.Random;
 //TODO: Capire se la view va istanziata nel controller oppure se va passata negli eventi che lancia al controller
 
 public class Game{
-    private ArrayList<Match> matches = new ArrayList<>();
-    Random rand = new Random();
+
+    private Match match;
 
     public Game(){
-        Match match = new Match();
-        matches.add(match);
-        //TODO communication with user, first example to be rewritten
-
+        match = new Match();
     }
 
     public void select(int i){
-        matches.get(0).createDashboard(i);
+        match.createDashboard(i);
     }
 
-    public String randomId(Random rand){
-        int i = 10000000 + rand.nextInt(89999999);
-        String id = Integer.toString(i);
-        for (Match match : this.matches){
-            //check to avoid different players have the same id
-            for(int index = 0; index< match.getPlayersSize(); index++){
-                if (match.getPlayerByIndex(index).getid().equals(id)){
-                    id=id+7;
-                }
-            }
-        }
-        if(Integer.parseInt(id)>99999999) id = randomId(rand);
-        return id;
-    }
-
-    public void addMatch(Match m){
-        if(!this.matches.contains(m)) this.matches.add(m);
-    }
-
-    public int getMatchesSize(){return this.matches.size();}
-
-    public Match getMatchByIndex(int index){return matches.get(index);}
-
-    public Match getMatch (int matchID) throws IllegalArgumentException{
-        for(Match m : this.matches){
-            if(m.getId()== matchID) return m;
-        }
-        throw new IllegalArgumentException();
-    }
-
-    public Random getRand(){return this.rand;}
-
-    public void startGame(int matchID){
-        Match match;
-        try{
-            match = this.getMatch(matchID);
-        } catch(IllegalArgumentException e) { return;}
+    public void startGame(){
         match.fillDashboard();
         match.getPlayerByIndex(0).setActive();
         for(int i=1; i<match.getPlayersSize(); i++){
             match.getPlayerByIndex(i).resetActive();
-        }
+    }
         match.firstTurnPows(); //assign two powcards to each players to start
     }
 
@@ -101,22 +62,6 @@ public class Game{
     private void resetTurn(Match match){
         for(Player p:match.getPlayers()) p.resetAction();
         match.getPlayerByIndex(0).setActive();
-    }
-
-    public static void main(String[] args){
-        Game g = new Game();
-
-        //Selection of the dashboard
-        System.out.println("player 1: choose a dashboard, 1, 2 or 3");
-        int choice=0;
-        try {
-            BufferedReader bufferedreader = new BufferedReader(new InputStreamReader(System.in));
-            String number = bufferedreader.readLine();
-            choice = Integer.parseInt(number);
-        }catch(IOException e)
-        {e.printStackTrace();}
-
-        g.select(choice); //method that creates a dashboard
     }
 }
 
--- src/main/java/network/server/GameRoom.java
+++ src/main/java/network/server/GameRoom.java
@@ -1,5 +1,8 @@
 package network.server;
 
+import controller.Game;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class GameRoom {
@@ -8,7 +11,33 @@ public class GameRoom {
 
     //TODO: capire se i messaggi vadano gestiti direttamente qua o inoltrati al controller
 
+
+    private GameServer server;
+    private Game gameController;
+    private ArrayList<String> players;
+
     public GameRoom(List<String> username){
-        System.out.println(username);
+        gameController=new Game();
+        players.addAll(username);
+        registerPlayers();
+        startGame();
     }
+
+
+    //Metodo necessario per istanziare effettivamente dei player nel model e darne conto al client
+    private void registerPlayers(){
+        chooseMap();                //Only for the first player
+    }
+
+    //Metodo necessario per la scelta della mappa, viene fatta richiesta a un solo client
+    private void chooseMap(){
+
+    }
+
+    //Metodo necessario ad istanziare tutto ciò che è necessario per il primo turno
+    private void startGame(){
+
+    }
+
+
 }
