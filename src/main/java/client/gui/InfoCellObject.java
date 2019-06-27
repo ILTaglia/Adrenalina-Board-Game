acb5d44a6d4d531cdaa674fdc132d24813e9bf16
Daniele Tagliabue <daniele1.tagliabue@mail.polimi.it>
2019-06-27 20:36:07 +0200

    Aggiunta parte relativa alla visualizzazione player nella mappa e pulsanti per oggetti nelle celle

---
+++ src/main/java/client/gui/InfoCellObject.java
@@ -0,0 +1,16 @@
+package client.gui;
+
+import model.Ammo;
+
+public class InfoCellObject {
+    Ammo ammo;
+    public InfoCellObject(Ammo ammo)
+    {
+        this.ammo=ammo;
+    }
+
+    public void mostra()
+    {
+
+    }
+}
