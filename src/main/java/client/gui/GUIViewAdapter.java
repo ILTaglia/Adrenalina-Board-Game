004e3121c99e5b6ff55cb109933e5c3ea7d85da3
Daniele Tagliabue <daniele1.tagliabue@mail.polimi.it>
2019-07-02 14:53:11 +0200

    Merge

--- src/main/java/client/gui/GUIViewAdapter.java
+++ src/main/java/client/gui/GUIViewAdapter.java
@@ -219,4 +219,40 @@ public class GUIViewAdapter implements View {
     @Override
     public void showWeaponInfo(Weapon weapon){}
 
+    @Override
+    public void getGunIndex() {
+
+    }
+
+    @Override
+    public void getSerieIndex() {
+
+    }
+
+    @Override
+    public void getPlayerIndex() {
+
+    }
+
+    @Override
+    public void getCellIndex() {
+
+    }
+
+    @Override
+    public void showPaymentError() {
+
+    }
+
+    @Override
+    public void getNextAttack() {
+
+    }
+
+    @Override
+    public void getDirection() {
+
+    }
+
+
 }
