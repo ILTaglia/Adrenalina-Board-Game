3d4ad0e6f536989d13211d4e8fb6694a54d91e84
AngelicaSofia <angelica.valeriani@mail.polimi.it>
2019-07-04 17:21:51 +0200

    Aggiunto model-UML completo

--- src/test/java/controller/ShootDoubleTest.java
+++ src/test/java/controller/ShootDoubleTest.java
@@ -64,8 +64,35 @@ public class ShootDoubleTest {
         System.out.println(player1.getWeaponByIndex(0).getAttack(0).getEffect(0).getType());
         System.out.println(player1.getWeaponByIndex(0).getAttack(0).getNumberEffect());
         shoot=new OfficialShootVersion(match, player1);
-        //shoot.
         int chosenindex=0;
         match.getActivePlayer().getWeaponByIndex(chosenindex);
+    }
+    @Test
+    public void test2(){
+        Ammo redAmmo = new Ammo(0);
+        Ammo bluAmmo = new Ammo(1);
+        Ammo yellowAmmo = new Ammo(2);
+
+        try{
+            player1.addAmmo(redAmmo);
+            player1.addAmmo(redAmmo);
+            player1.addAmmo(bluAmmo);
+            player1.addAmmo(bluAmmo);
+            player1.addAmmo(yellowAmmo);
+            player1.addAmmo(yellowAmmo);
+        }
+        catch(MoreThanTreeAmmosException e){}
+        try{grabweapon.grabWeapon(match, player1, 0);}
+        catch(MaxNumberofCardsException e){}
+        System.out.println(player1.getWeaponByIndex(0).getName());
+        System.out.println(player1.getWeaponByIndex(0).getCostToRecharge());
+        System.out.println(player1.getWeaponByIndex(0).getCostToGrab());
+        System.out.println(player1.getWeaponByIndex(0).getNumberAttack());
+        System.out.println(player1.getWeaponByIndex(0).getAttack(0).getEffect(0).getType());
+        System.out.println(player1.getWeaponByIndex(0).getAttack(0).getNumberEffect());
+        shoot=new OfficialShootVersion(match, player1);
+        int chosenindex=0;
+        match.getActivePlayer().getWeaponByIndex(chosenindex);
+        //assertTrue(shoot.getlistattackable(1).contains(player2));
     }
 }
